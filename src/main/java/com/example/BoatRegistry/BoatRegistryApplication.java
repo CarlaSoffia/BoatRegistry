ad45dd79e6ac943902a81f670a7dfe732a0ff832
Carla Sofia Mendes <carlasofiamendes@outlook.com>
2025-06-04 22:59:08 +0100

    Create project

---
+++ src/main/java/com/example/BoatRegistry/BoatRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.BoatRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatRegistryApplication.class, args);
+	}
+
+}
