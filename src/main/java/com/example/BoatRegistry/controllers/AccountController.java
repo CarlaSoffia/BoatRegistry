3c641ba3f188a396419885c740fe5e399d90aa05
zizare <gabinmatthieu@gmail.com>
2025-06-19 16:51:05 +0200

    Implement JWT authentication and authorization in AccountController and SecurityConfig

--- src/main/java/com/example/BoatRegistry/controllers/AccountController.java
+++ src/main/java/com/example/BoatRegistry/controllers/AccountController.java
@@ -5,24 +5,42 @@ import com.example.BoatRegistry.dtos.authentication.AuthenticationResponseDto;
 import com.example.BoatRegistry.security.JwtHelper;
 import com.example.BoatRegistry.services.UserService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
 @RestController
 public class AccountController {
 
-    private final AuthenticationManager authenticationManager;
+//    private final AuthenticationManager authenticationManager;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final UserService userService;
+
+    private final JwtEncoder jwtEncoder;
     private final JwtHelper jwtHelper;
 
-    public AccountController(AuthenticationManager authenticationManager, UserService userService, JwtHelper jwtHelper) {
-        this.authenticationManager = authenticationManager;
+    public AccountController(final AuthenticationManagerBuilder authenticationManagerBuilder, UserService userService, final JwtEncoder jwtEncoder, JwtHelper jwtHelper) {
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.userService = userService;
+        this.jwtEncoder = jwtEncoder;
         this.jwtHelper = jwtHelper;
     }
 
@@ -30,12 +48,46 @@ public class AccountController {
     public ResponseEntity<AuthenticationResponseDto> login(@Valid @RequestBody AuthenticationRequestDto authenticationRequestDto) {
         var email = authenticationRequestDto.getEmail();
         userService.anyWithEmail(email);
-        var authentication = new UsernamePasswordAuthenticationToken(email, authenticationRequestDto.getPassword());
-        authenticationManager.authenticate(authentication);
         String token = jwtHelper.generateToken(email);
         var authenticationResponseDto = new AuthenticationResponseDto();
         authenticationResponseDto.setEmail(email);
         authenticationResponseDto.setToken(token);
         return ResponseEntity.status(HttpStatus.OK).body(authenticationResponseDto);
     }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponseDto> authorize(@Valid @RequestBody AuthenticationRequestDto loginVM) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                loginVM.getEmail(),
+                loginVM.getPassword()
+        );
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = this.createToken(authentication, false);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setBearerAuth(jwt);
+        var authenticationResponseDto = new AuthenticationResponseDto();
+        authenticationResponseDto.setEmail(loginVM.getEmail());
+        authenticationResponseDto.setToken(jwt);
+        return new ResponseEntity<>(authenticationResponseDto, httpHeaders, HttpStatus.OK);
+    }
+
+    public String createToken(Authentication authentication, boolean rememberMe) {
+
+        Instant now = Instant.now();
+        Instant validity;
+        validity = now.plus(60, ChronoUnit.MINUTES);
+
+        // @formatter:off
+        JwtClaimsSet.Builder builder = JwtClaimsSet.builder()
+                .issuedAt(now)
+                .expiresAt(validity)
+                .subject(authentication.getName())
+                .claim("auth", "authorities , authorities, piong");
+
+        JwsHeader jwsHeader = JwsHeader.with(MacAlgorithm.HS512).build();
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader, builder.build())).getTokenValue();
+    }
+
 }
--- src/main/java/com/example/BoatRegistry/security/SecurityConfig.java
+++ src/main/java/com/example/BoatRegistry/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.BoatRegistry.security;
 
 import com.example.BoatRegistry.services.UserService;
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,13 +14,27 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
     private final UserDetailsServiceImp userDetailsService;
     private final JwtAuthenticationHelper jwtAuthenticationHelper;
+
+    @Value("${jwt.secret}")
+    private String jwtKey;
+
     public SecurityConfig(UserDetailsServiceImp userDetailsService, JwtAuthenticationHelper jwtAuthenticationHelper) {
         this.userDetailsService = userDetailsService;
         this.jwtAuthenticationHelper = jwtAuthenticationHelper;
@@ -29,23 +46,33 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-        return authenticationManagerBuilder.build();
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
     }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(getSecretKey()).macAlgorithm(MacAlgorithm.HS512).build();
+        return jwtDecoder::decode;
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.from(jwtKey).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, MacAlgorithm.HS512.getName());
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login", "/authenticate").permitAll()
                         .anyRequest().authenticated())
-                .authenticationManager(authenticationManager)
-                .addFilterBefore(jwtAuthenticationHelper, UsernamePasswordAuthenticationFilter.class)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
                 .build();
     }
 }
